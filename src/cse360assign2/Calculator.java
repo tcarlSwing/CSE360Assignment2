4e5bea9a3d5967be37a3c66f8524847da9f31b79
Thomas <tom.c.shadow@gmail.com>
2019-02-22 16:25:16 -0700

    Creating the initial version: initializing the project.

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,75 @@
+/**
+ * Author: Thomas Duke
+ * Class ID: 367
+ * Assignment #: 2
+ * 
+ * Implements a Calculator class to perform basic operations
+ * and retrieve operation history.
+ */
+package cse360assign2;
+
+/**
+ * Defines the Calculator class.
+ * Calculator operations include: add, subtract, multiply, divide.
+ * Additional functions: getTotal and getHistory.
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor for the Calculator class.
+	 * Performs all necessary initialization.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total value computed by the calculator.
+	 * @return The total value.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds value to this object's total.
+	 * @param value The value to add
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts value from this object's total.
+	 * @param value The value to subtract
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies value to this object's total.
+	 * @param value The value to multiply
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides this object's total by value.
+	 * @param value The value to divide total by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Retrieves and yields the command history for this object.
+	 * @return This calculator's command history.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
